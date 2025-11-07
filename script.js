19bd29151d6ef02831b0d49f8fe5f77eb0fd16a5
hasitha-ramesh <hasitha.ict@gmail.com>
2025-11-07 09:41:40 +0530

    Added questions and Answers

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+  {
+    question: "What is the capital of Sri Lanka?",
+    answers: [
+      { text: "Jayawardenepura Kotte", correct: true },
+      { text: "Kandy", correct: false },
+      { text: "Jaffna", correct: false },
+      { text: "Galle", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which large mammal is commonly found in Sri Lanka's wildlife reserves?",
+    answers: [
+      { text: "Elephant", correct: true },
+      { text: "Tiger", correct: false },
+      { text: "Lion", correct: false },
+      { text: "Kangaroo", correct: false },
+    ],
+  },
+  {
+    question: "What is the main religion of Sri Lanka?",
+    answers: [
+      { text: "Buddhism", correct: true },
+      { text: "Christianity", correct: false },
+      { text: "Hinduism", correct: false },
+      { text: "Islam", correct: false },
+    ],
+  },
+  {
+    question: "Name a popular rice flour dish in Sri Lanka?",
+    answers: [
+      { text: "Hoppers", correct: true },
+      { text: "Pizza", correct: false },
+      { text: "Sushi", correct: false },
+      { text: "Tacos", correct: false },
+    ],
+  },
+];
