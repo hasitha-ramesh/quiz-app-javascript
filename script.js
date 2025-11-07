df1a9ab1df8e53ce8286b3d357991ec401f30429
hasitha-ramesh <hasitha.ict@gmail.com>
2025-11-07 18:48:48 +0530

    Add basic quiz app structure with HTML layout and JS question rendering

--- script.js
+++ script.js
@@ -37,3 +37,40 @@ const questions = [
     ],
   },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-btns");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let QuestionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = QuestionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+  });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
